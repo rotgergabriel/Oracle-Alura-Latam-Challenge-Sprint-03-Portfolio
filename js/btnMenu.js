d02b8008c1130757df3d50426005f9e1fdb47d26
Gabriel Rotger <rotgergabriel@gmail.com>
2022-11-15 18:19:54 +0100

    Fixed mobile styles and button functionality with js

---
+++ js/btnMenu.js
@@ -0,0 +1,23 @@
+   
+const btnMenu = document.querySelector('[data-btnMenu]')
+const btnMenuClose = document.querySelector('[data-btnMenu-close]')
+const headerLinks = document.querySelector('[data-links]')
+
+btnMenu.addEventListener('click', () => {
+    btnMenu.style.display = 'none'
+    headerLinks.style.display = 'flex'
+    btnMenuClose.style.visibility = 'visible'
+})
+
+
+btnMenuClose.addEventListener('click', () => {
+    btnMenuClose.style.visibility = 'hidden'
+    btnMenu.style.display = ''
+    headerLinks.style.display = 'none'
+})
+
+headerLinks.addEventListener('click', () => {
+    headerLinks.style.display = 'none'
+    btnMenu.style.display = ''
+    btnMenuClose.style.visibility = 'hidden'
+})
