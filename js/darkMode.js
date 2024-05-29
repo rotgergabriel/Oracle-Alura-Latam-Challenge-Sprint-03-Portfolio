98bc4505641cdaaf7231c43e90b63007c0d0ca44
Gabriel Rotger <rotgergabriel@gmail.com>
2024-05-29 02:20:55 +0200

    Various changes: Cv file, profile.jpeg, home.html, darkMode.js, db.js, index.js, main.css, loader.css

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,6 +2,10 @@
 
     const btnSwitch = document.querySelector('[data-switch]')
 
+    // Agrega la clase 'dark-mode' al cargar la página
+    document.body.classList.add('dark-mode')
+    btnSwitch.classList.add('active')
+
     btnSwitch.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode')
         btnSwitch.classList.toggle('active')
