49c1ba814527c902d53a6582e277b408df4848cc
Gabriel Rotger <rotgergabriel@gmail.com>
2025-10-13 21:49:16 +0200

    Update download attribute for CV link to match file name

--- js/block/main.js
+++ js/block/main.js
@@ -10,7 +10,7 @@ profile.innerHTML = data.profile.map(element =>
         <div class="main__links">
                 <li><a class="main__link underline" href=${element.github} target="_blank">GitHub<span>&#9701</span></a></li>
                 <li><a class="main__link underline" href=${element.linkedin} target="_blank">Linkedin<span>&#9701</span></a></li>
-                <li><a class="main__link underline" href="/assets/file/CV_Rotger_Gabriel_Junior_Fronted_Developer.pdf" download="Cv-Rotger">Cv-Download<span>&#9701</span></a></li>
+                <li><a class="main__link underline" href="/assets/file/CV_Rotger_Gabriel_Junior_Fronted_Developer.pdf" download="CV_Rotger_Gabriel_Junior_Fronted_Developer.pdf">Cv-Download<span>&#9701</span></a></li>
         </div>
     </div>`
 ).join('')
